0ef08373b259d352113351bbe9bf2c7d17ebec00
Maximilian Hofmann <mh@wildemail.de>
2022-12-03 01:04:29 +0100

    ✨  some refactoring & small improvements
    Signed-off-by: Maximilian Hofmann <mh@wildemail.de>

--- src/day01/Day01.kt
+++ src/day01/Day01.kt
@@ -5,35 +5,35 @@ import readInput
 @OptIn(ExperimentalUnsignedTypes::class)
 fun main() {
 
-    /**
-     * This is the first part
-     */
     val input = readInput("day01/input")
 
-    val calories = input.map { it.toUShortOrNull() }
-
-    var sum: UInt = 0u
     var sumArray: UIntArray = uintArrayOf()
 
-    /**
-     * If the current value is a number, add it to the sum
-     * Else push the sum to the sum array and reset the sum value to null
-     */
-    for (cal in calories) {
-        if (cal is UShort) {
-            sum += cal
-        } else {
-            sumArray += sum
-            sum = 0u
-        }
+    fun part1(input: List<String>): UInt {
+        val calories = input.map { it.toUShortOrNull() }
+
+        var sum: UInt = 0u
+
+
+        /**
+         * If the current value is a number, add it to the sum
+         * Else push the sum to the sum array and reset the sum value to null
+         */
+        for (cal in calories)
+            if (cal is UShort) {
+                sum += cal
+            } else {
+                sumArray += sum
+                sum = 0u
+            }
+        return sumArray.max()
     }
-    println(sumArray.max())
 
+    fun part2(): UInt {
+        val sortedCals = sumArray.sortedArrayDescending()
+        return sortedCals[0] + sortedCals[1] + sortedCals[2]
+    }
 
-    /**
-     * And this is the second part
-     */
-    val sortedCals = sumArray.sorted().reversed()
-    val topCalsSum = sortedCals[0] + sortedCals[1] + sortedCals[2]
-    println(topCalsSum)
+    println(part1(input))
+    println(part2())
 }
